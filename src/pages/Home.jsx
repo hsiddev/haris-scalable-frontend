1e269f6373c7172327497786ac3797a1d3daac9b
Haris Siddiqui <hs8932079@gmail.com>
2026-01-12 06:38:01 +0000

    fix: update header title and simplify user greeting in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,11 +49,11 @@ const Home = () => {
     <div className="home-container">
       <header className="header">
         <div className="header-content">
-          <h1>Haris Assignment - Scalable Assignment</h1>
+          <h1>Haris Assignment - Scalable Solutions Assignment</h1>
           <div className="header-actions">
             {user ? (
               <>
-                <span className="user-info">Welcome, {user.username}, {user.name}</span>
+                <span className="user-info">Welcome, {user.username}</span>
                 {user.role === 'creator' && (
                   <Link to="/creator/upload" className="btn-primary">
                     Upload Photo
